8f11bce81dc51280f3fab2ecc4e4455632587aa0
李国豪 <lcliguohao@163.com>
2015-01-09 00:47:16 +0800

    create_new.sql added

---
+++ src/create_new.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE orderDB;
+USE orderDB;
+CREATE TABLE book (  id int(10) NOT NULL,  title varchar(100) NOT NULL,  authors varchar(200),  publisher_id int(10) NOT NULL,  copies int(10),  PRIMARY KEY  (id));
